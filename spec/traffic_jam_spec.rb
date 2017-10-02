4932b98e9f20716cf7813f149f7a5fdd416c3c01
Ashley Martens <ashley@coinbase.com>
2017-10-02 15:53:31 -0700

    Did not need to change that variable name

--- spec/traffic_jam_spec.rb
+++ spec/traffic_jam_spec.rb
@@ -9,60 +9,60 @@ describe TrafficJam do
     config.register(:test4, 4, 60)
   end
 
-  let(:user) { "user1" }
+  let(:value) { "user1" }
 
   describe '::limit' do
     it "should return limit instance with registered limits" do
-      limit = TrafficJam.limit(:test, user)
+      limit = TrafficJam.limit(:test, value)
       assert_equal :test, limit.action
-      assert_equal user, limit.value
+      assert_equal value, limit.value
       assert_equal 3, limit.max
       assert_equal 60, limit.period
     end
 
     it "should raise error if not found" do
       assert_raises(TrafficJam::LimitNotFound) do
-        TrafficJam.limit(:test2, user)
+        TrafficJam.limit(:test2, value)
       end
     end
   end
 
   describe '::reset_all' do
     it "should reset all rate limits" do
-      limit = TrafficJam.increment!(:test, user)
-      limit = TrafficJam.increment!(:test4, user)
-      assert_equal 1, TrafficJam.used(:test, user)
-      assert_equal 1, TrafficJam.used(:test4, user)
+      limit = TrafficJam.increment!(:test, value)
+      limit = TrafficJam.increment!(:test4, value)
+      assert_equal 1, TrafficJam.used(:test, value)
+      assert_equal 1, TrafficJam.used(:test4, value)
 
       TrafficJam.reset_all
-      assert_equal 0, TrafficJam.used(:test, user)
-      assert_equal 0, TrafficJam.used(:test4, user)
+      assert_equal 0, TrafficJam.used(:test, value)
+      assert_equal 0, TrafficJam.used(:test4, value)
     end
 
     it "should reset all rate limits for one action" do
-      limit = TrafficJam.increment!(:test, user)
-      limit = TrafficJam.increment!(:test4, user)
-      assert_equal 1, TrafficJam.used(:test, user)
-      assert_equal 1, TrafficJam.used(:test4, user)
+      limit = TrafficJam.increment!(:test, value)
+      limit = TrafficJam.increment!(:test4, value)
+      assert_equal 1, TrafficJam.used(:test, value)
+      assert_equal 1, TrafficJam.used(:test4, value)
 
       TrafficJam.reset_all(action: :test)
-      assert_equal 0, TrafficJam.used(:test, user)
-      assert_equal 1, TrafficJam.used(:test4, user)
+      assert_equal 0, TrafficJam.used(:test, value)
+      assert_equal 1, TrafficJam.used(:test4, value)
     end
   end
 
   describe 'class helpers' do
     before { TrafficJam.config.register(:test, 3, 60) }
-    let(:user) { "user1" }
+    let(:value) { "user1" }
 
     it "should call methods with registered limits" do
-      TrafficJam.increment(:test, user, 1)
-      assert_equal 1, TrafficJam.used(:test, user)
+      TrafficJam.increment(:test, value, 1)
+      assert_equal 1, TrafficJam.used(:test, value)
     end
 
     it "should raise error if limit not found" do
       assert_raises(TrafficJam::LimitNotFound) do
-        TrafficJam.increment(:test2, user, 1)
+        TrafficJam.increment(:test2, value, 1)
       end
     end
   end
