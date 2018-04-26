92a7f181fddef5f4d2a0108989e6378f94436ecf
Jordan Sitkin <jordan.sitkin@coinbase.com>
2018-04-25 22:53:34 -0700

    sketching out GCRA implementation

---
+++ lib/traffic_jam/gcra_limit.rb
@@ -0,0 +1,62 @@
+require_relative 'limit'
+require_relative 'scripts'
+
+module TrafficJam
+  # TODO add some nice prose about GRCA
+  #
+  # Example: Limit is 5 per 10 seconds.
+  #          Explain how this works!
+  class GCRALimit < Limit
+    # Increment the amount used by the given number. Does not perform increment
+    # if the operation would exceed the limit. Returns whether the operation was
+    # successful.
+    #
+    # @param amount [Integer] amount to increment by
+    # @param time [Time] time is ignored
+    # @return [Boolean] true if increment succeeded and false if incrementing
+    #   would exceed the limit
+    def increment(amount = 1, time: Time.now)
+      return true if amount == 0
+      return false if max == 0
+      raise ArgumentError.new("Amount must be positive") if amount < 0
+      raise ArgumentError.new("Amount must be an integer") if amount != amount.to_i
+      return false if amount > max
+      !!run_increment_script(key, [max, max, period, amount])
+    end
+
+    # Decrement the amount used by the given number.
+    #
+    # @param amount [Integer] amount to decrement by
+    # @param time [Time] time is ignored
+    # @raise [NotImplementedError] decrement is not defined for SimpleLimit
+    def decrement(_amount = 1, time: Time.now)
+      raise NotImplementedError, "decrement is not defined for GRCALimit"
+    end
+
+    # Return amount of limit used, taking time drift into account.
+    #
+    # @return [Integer] amount used
+    def used
+      return 0 if max.zero?
+      run_read_script(key, [max, max, period])
+    end
+
+    def key_prefix
+      "#{config.key_prefix}:s"
+    end
+
+    private
+
+    def run_increment_script(key, argv)
+      redis.evalsha(Scripts::INCREMENT_GCRA_HASH, keys: [key], argv: argv)
+    rescue Redis::CommandError
+      redis.eval(Scripts::INCREMENT_GCRA, keys: [key], argv: argv)
+    end
+
+    def run_read_script(key, argv)
+      redis.evalsha(Scripts::READ_GCRA_HASH, keys: [key], argv: argv)
+    rescue Redis::CommandError
+      redis.eval(Scripts::READ_GCRA, keys: [key], argv: argv)
+    end
+  end
+end
---
+++ scripts/increment_gcra.lua
@@ -0,0 +1,44 @@
+-- this script has side-effects, so it requires replicate commands mode
+redis.replicate_commands()
+
+local burst = ARGV[1]
+local rate = ARGV[2]
+local period = ARGV[3]
+local cost = ARGV[4]
+
+local emission_interval = period / rate
+local increment = emission_interval * cost
+local burst_offset = emission_interval * burst
+local now = redis.call("TIME")
+
+-- redis returns time as an array containing two integers: seconds of the epoch
+-- time (10 digits) and microseconds (6 digits). for convenience we need to
+-- convert them to a floating point number. the resulting number is 16 digits,
+-- bordering on the limits of a 64-bit double-precision floating point number.
+-- adjust the epoch to be relative to Jan 1, 2017 00:00:00 GMT to avoid floating
+-- point problems. this approach is good until "now" is 2,483,228,799 (Wed, 09
+-- Sep 2048 01:46:39 GMT), when the adjusted value is 16 digits.
+local jan_1_2017 = 1483228800
+now = (now[1] - jan_1_2017) + (now[2] / 1000000)
+
+local tat = redis.call("GET", KEYS[1])
+
+if not tat then
+  tat = now
+else
+  tat = tonumber(tat)
+end
+
+local new_tat = math.max(tat, now) + increment
+local allow_at = new_tat - burst_offset
+local diff = now - allow_at
+
+local remaining = math.floor(diff / emission_interval + 0.5) -- poor man's round
+
+if remaining < 0 then
+  return false
+end
+
+local reset_after = new_tat - now
+redis.call("SET", KEYS[1], new_tat, "EX", math.ceil(reset_after))
+return true
